584136031e0e9aa9e75088fdf8e388dcd8a27c2f
lTITANl <101052690+lTITANl@users.noreply.github.com>
2023-10-04 16:15:19 +0900

    Update dec2hex.c

--- dec2hex.c
+++ dec2hex.c
@@ -7,23 +7,33 @@ typedef unsigned char* pointer;
 bool is64bit()
 {
 	return sizeof(void*) == 8;
+	//sizeof(void*)이 32비트 운영체제에서는 4비트, 64에서는 8비트
 }
 
-bool isBigEndian(){
-	return true;
+bool isBigEndian() {
+    unsigned int testValue = 1;
+    unsigned char* bytePointer = (unsigned char*)&testValue;
+    return bytePointer[0] == 0;
 }
 
-void le_show_bytes()
-{
-	printf("0xFFFFFFFF\n");
-	return;
-}
 
-void be_show_bytes(){
-	printf("0xFFFFFFFF\n");
-	return;
+void le_show_bytes(pointer ptr, size_t size) {
+    printf("0x");
+    for (int i = size - 1; i >= 0; i--) {
+        printf("%.2X", ptr[i]);
+    }
+    printf("\n");
 }
+//little endian은 낮은 주소에 데이터의 낮은 바이트(lsb)를 저장
 
+void be_show_bytes(pointer ptr, size_t size) {
+    printf("0x");
+    for (size_t i = 0; i < size; i++) {
+        printf("%.2X", ptr[i]);
+    }
+    printf("\n");
+}
+//big endian은 낮은 주소에 데이터의 큰 바이트를 저장
 
 int main(int argc, char* argv[])
 {
@@ -37,8 +47,7 @@ int main(int argc, char* argv[])
 	printf("ORDERING=%s\n", isBigEndian()? "BIG_ENDIAN": "LITTLE_ENDIAN");
 
 	printf("MYANS: DEC=%d HEX=", a);
-	isBigEndian()? be_show_bytes(): le_show_bytes();
-		//be_show_bytes((pointer)&a, sizeof(unsigned int)): le_show_bytes((pointer)(&a), sizeof(unsigned int));
+	isBigEndian()? be_show_bytes((pointer)&a, sizeof(unsigned int)): le_show_bytes((pointer)(&a), sizeof(unsigned int));
 
 	printf("CHECK: DEC=%d HEX=%.8X\n", a, a);
 	return 0;
